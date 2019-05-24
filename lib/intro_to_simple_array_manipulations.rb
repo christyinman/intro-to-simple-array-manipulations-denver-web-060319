a745798e7aa378399249ffdae0ac1894a7c3acec
Christy Inman <christyinman@ymail.com>
2019-05-24 14:39:28 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -23,8 +23,8 @@ def shift_with_args(array)
 end  
 
 def using_concat(array_1, array_2)
-  new_array = []
-  new_array.concat(array_1 + array_2)
+  #new_array = []
+  array_1.concat(array_1 + array_2)
 end  
 
 def using_insert(array, new_ele)
